7aa6a89a877c018ca353e8531763f4ef0ff0b78b
空雲 <info@croud.jp>
2021-01-31 16:23:53 +0900

    update tsdoc

--- src/Components/VirtualWindow/VirtualWindow.tsx
+++ src/Components/VirtualWindow/VirtualWindow.tsx
@@ -24,34 +24,181 @@ import { useWindow } from "../../Hooks/useWindow";
  * @interface Props
  */
 interface Props {
+  /**
+   * Window title
+   *
+   * @type {ReactNode}
+   * @memberof Props
+   */
   title?: ReactNode;
+  /**
+   * Whether to display the title
+   *
+   * @type {boolean}
+   * @memberof Props
+   */
   titleEnable?: boolean;
+  /**
+   * Title bar size
+   *
+   * @type {number}
+   * @memberof Props
+   */
   titleSize?: number;
+  /**
+   * Presence or absence of a button attached to the title
+   *
+   * @type {({ [key in "min" | "max" | "close"]?: boolean })}
+   * @memberof Props
+   */
   titleButtons?: { [key in "min" | "max" | "close"]?: boolean };
+  /**
+   * Whether to activate in the initial state
+   *
+   * @type {boolean}
+   * @memberof Props
+   */
   active?: boolean;
+  /**
+   * Whether to set position to fixed
+   *
+   * @type {boolean}
+   * @memberof Props
+   */
   overlapped?: boolean;
+  /**
+   * Placement criteria in the X direction
+   *
+   * @type {BaseType}
+   * @memberof Props
+   */
   baseX?: BaseType;
+  /**
+   * Placement criteria in the Y direction
+   *
+   * @type {BaseType}
+   * @memberof Props
+   */
   baseY?: BaseType;
+  /**
+   * Initial X position
+   *
+   * @type {number}
+   * @memberof Props
+   */
   x?: number;
+  /**
+   * Initial Y position
+   *
+   * @type {number}
+   * @memberof Props
+   */
   y?: number;
+  /**
+   * Initial width
+   *
+   * @type {number}
+   * @memberof Props
+   */
   width?: number;
+  /**
+   * Initial height
+   *
+   * @type {number}
+   * @memberof Props
+   */
   height?: number;
-  child?: boolean;
+  /**
+   * Window state
+   *
+   * @type {WindowState}
+   * @memberof Props
+   */
   state?: WindowState;
+  /**
+   * Frame size
+   *
+   * @type {number}
+   * @memberof Props
+   */
   frameSize?: number;
+  /**
+   * Whether to allow resizing
+   *
+   * @type {boolean}
+   * @memberof Props
+   */
   resize?: boolean;
+  /**
+   * Invisible frame size for resizing
+   *
+   * @type {number}
+   * @memberof Props
+   */
   resizeBold?: number;
+  /**
+   * Client style
+   *
+   * @type {React.CSSProperties}
+   * @memberof Props
+   */
   clientStyle?: React.CSSProperties;
+  /**
+   * Client class name
+   *
+   * @type {string}
+   * @memberof Props
+   */
   clientClass?: string;
+  /**
+   * Whether the client can be dragged and dropped
+   *
+   * @type {boolean}
+   * @memberof Props
+   */
   clientMovable?: boolean;
+  /**
+   * Dispatch for parameter setting
+   *
+   * @type {(MutableRefObject<Dispatch<ActionType> | null>)}
+   * @memberof Props
+   */
   dispatch?: MutableRefObject<Dispatch<ActionType> | null>;
+  /**
+   * State change event
+   *
+   * @memberof Props
+   */
   onUpdate?: (params: WindowParams) => void;
 }
 
 /**
- * Window component
  *
- * @param {Props}
+ *
+ * @param {*} {
+ *   title = "",
+ *   titleSize = 32,
+ *   titleEnable = true,
+ *   titleButtons,
+ *   active = false,
+ *   overlapped = true,
+ *   x = 0,
+ *   y = 0,
+ *   baseY = "start",
+ *   baseX = "start",
+ *   width = 640,
+ *   height = 480,
+ *   state = "normal",
+ *   frameSize = 1,
+ *   resize = true,
+ *   resizeBold = 8,
+ *   clientStyle,
+ *   clientClass,
+ *   clientMovable = false,
+ *   children,
+ *   dispatch: refDispatch,
+ *   onUpdate,
+ * }
  * @return {*}
  */
 export const VirtualWindow: FC<Props> = ({
